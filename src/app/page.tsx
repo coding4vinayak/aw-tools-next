1fbc3c6da19b708c9ec1570f4124907266b53d90
vinayakss007 <vinayak.shruti.biz@gmail.com>
2026-03-02 22:58:33 +0530

    Fix root URL redirect to /index.html - remove header condition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export const dynamic = 'force-dynamic';
-
 export default function Home() {
-  // Redirect to the static tools index page
   redirect('/index.html');
 }
